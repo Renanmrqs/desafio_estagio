2f85b1880613543582084fea9e2c955dd1f260f7
Renanmrqs <renanmarques1923@gmail.com>
2026-04-10 18:18:22 -0300

    fibonacci recursivo

--- fibonacci.py
+++ fibonacci.py
@@ -6,7 +6,7 @@ EX. fib(0) =0; fib(1) = 1; fib(2) = 1; fib(3) = 2; fib(5) = 5; fib(6) = 8.
 --- Criar uma função recursiva que resolva Fibonacci 
 --- Criar uma função linear que resolva Fibonacci
 """
-def fibonacci_recursivo():
+def linear():
     num = valid_input(input('Digite um numero: '))
     if num == 0:
         fib = 0
@@ -20,7 +20,18 @@ def fibonacci_recursivo():
         num1 = num2
         num2 = fib   
     return fib
-        
+
+def recursivo():
+    num = valid_input(input('Digite um numero: '))
+    return fibonacci_recursivo(num)
+    
+
+def fibonacci_recursivo(num):
+    if num <= 1:
+        return num
+    else:
+        return fibonacci_recursivo(num - 1) + fibonacci_recursivo(num - 2)
+    
 
 def valid_input(n):
     try:
@@ -32,4 +43,6 @@ def valid_input(n):
         return 'escreva um numero inteiro'
     
     
-print(fibonacci_recursivo())
+
+print(linear())
+print(recursivo())
